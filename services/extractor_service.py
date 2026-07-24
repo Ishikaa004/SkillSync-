0c04cdcb2542afedf6199f9a439c0966eedb7f93
Ishikaa004 <ishikapal04@gmail.com>
2026-07-24 23:57:45 +0530

    Day 9: Implement skill extraction

--- services/extractor_service.py
+++ services/extractor_service.py
@@ -1,5 +1,5 @@
 import re
-
+from pathlib import Path
 
 def extract_email(text):
 
@@ -27,7 +27,32 @@ def extract_resume_details(text):
 
     details = {
         "email": extract_email(text),
-        "phone": extract_phone(text)
+        "phone": extract_phone(text),
+        "skills": extract_skills(text)
     }
 
     return details
+
+def load_skills():
+
+    skills_file = Path("../data/skills.txt")
+
+    with open(skills_file, "r") as file:
+        skills = file.read().splitlines()
+
+    return skills
+
+def extract_skills(text):
+
+    skills = load_skills()
+
+    found_skills = []
+
+    text = text.lower()
+
+    for skill in skills:
+
+        if skill.lower() in text:
+            found_skills.append(skill)
+
+    return found_skills
